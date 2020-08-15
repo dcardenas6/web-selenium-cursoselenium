087d8a83c791eabc994d045f824fa817cc76258e
dcardenas6 <69393850+dcardenas6@users.noreply.github.com>
2020-08-15 12:02:07 -0500

    Segundo commit. Tipos de datos y operadores

---
+++ SeleniumAutomation/src/test/java/com/javabasics/Datatypes.java
@@ -0,0 +1,65 @@
+package com.javabasics;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Datos numéricos enteros
+		 */
+		
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		x=127;
+//		y=32767;
+//		z=2147483647;
+//		a=9223372036854775807L;
+//		
+//		System.out.println(y);
+
+		
+		/*
+		 * Datos numéricos reales
+		 */
+		
+//		float base;
+//		double altura;
+//		
+//		base = 30.54f;
+//		altura = 40.76;
+//		
+//		System.out.println(base+altura);
+		
+		
+		/*
+		 * Datos caracteres
+		 */
+		
+//		char letra1;
+//		char letra2;
+//		String frase;
+//		
+//		letra1='a';
+//		letra2='\u0047';
+//		frase="Hola mundo!";
+//		System.out.println(frase);
+		
+		
+		/*
+		 * Datos lógicos
+		 */
+		
+//		boolean avesvuelan;
+//		
+//		avesvuelan=true;
+//		
+//		System.out.println("las aves vuelan? "+avesvuelan);
+		
+	
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasics/Operadores.java
@@ -0,0 +1,48 @@
+package com.javabasics;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Aritméticos
+		 */
+		
+//		int x = 1;
+//		int y = 2;
+//		int suma;
+//		
+//		suma=y+x;
+//		
+//		System.out.println(suma);
+
+		
+		/*
+		 * Unarios
+		 */
+		
+//		int valor1 = 4;
+//		int incrementar = ++ valor1;
+//		
+//		System.out.println(incrementar);
+		
+		/*
+		 * Relacionales y condicionales
+		 */
+		
+//		int num1 = 2;
+//		int num2 = 4;
+//		int num3 = 8;
+//		int num4 = 12;
+//		
+//		if (num1 >= 5 || num2 >= 5){
+//			System.out.println("un valor es mayor o igual a 5");
+//		}else {
+//			System.out.println("ninguno es 5 o más");
+//		}
+		
+		
+	}
+
+}
