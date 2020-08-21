c40d9e69cdd098457742b6a203feaa08ab888a35
dcardenas6 <69393850+dcardenas6@users.noreply.github.com>
2020-08-20 19:12:58 -0500

    Segundo commit desde otro equipo

--- SeleniumAutomation/src/test/java/com/javabasics/Datatypes.java
+++ SeleniumAutomation/src/test/java/com/javabasics/Datatypes.java
@@ -9,17 +9,17 @@ public class Datatypes {
 		 * Datos numéricos enteros
 		 */
 		
-		byte x;
-		short y;
-		int z;
-		long a;
-		
-		x=127;
-		y=32767;
-		z=2147483647;
-		a=9223372036854775807L;
-		
-		System.out.println(y);
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		x=127;
+//		y=32767;
+//		z=2147483647;
+//		a=9223372036854775807L;
+//		
+//		System.out.println(y);
 
 		
 		/*
@@ -39,14 +39,14 @@ public class Datatypes {
 		 * Datos caracteres
 		 */
 		
-//		char letra1;
-//		char letra2;
-//		String frase;
-//		
-//		letra1='a';
-//		letra2='\u0047';
-//		frase="Hola mundo!";
-//		System.out.println(frase);
+		char letra1;
+		char letra2;
+		String frase;
+		
+		letra1='a';
+		letra2='\u0047';
+		frase="Hola mundo!";
+		System.out.println(frase);
 		
 		
 		/*
